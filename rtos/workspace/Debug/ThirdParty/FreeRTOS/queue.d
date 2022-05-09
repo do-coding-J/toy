02f7da05d6d0ce5082f0a9e306523f842478d143
do-coding-J <ghks9705@gmail.com>
2022-05-09 14:23:06 +0900

    rtos

---
+++ rtos/workspace/Debug/ThirdParty/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+ThirdParty/FreeRTOS/queue.o: ../ThirdParty/FreeRTOS/queue.c \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/portable.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/task.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/list.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/queue.h \
+ C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/task.h
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/portable.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/task.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/list.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/queue.h:
+C:/drive/toy/rtos/workspace/ThirdParty/FreeRTOS/include/task.h:
